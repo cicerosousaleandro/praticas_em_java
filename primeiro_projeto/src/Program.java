36e2e5cee0839e591f122d2d121c0888e5fdc181
Cícero Sousa <cicerosousa.le@gmail.com>
2026-04-17 00:26:44 -0300

    Saída de dados em JAVA, processamento de dados em JAVA.

--- primeiro_projeto/src/Program.java
+++ primeiro_projeto/src/Program.java
@@ -1,8 +1,40 @@
+import java.util.Locale;
 
 public class Program {
 
-	public static void main(String[] args) {
-		System.out.println("Bem Vindo");
-	}
-	
+    public static void main(String[] args) {
+        
+        // Declaração das variáveis 
+        String product1 = "Computer";
+        String product2 = "Office desk";
+        
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';  
+        
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+        
+        // Configura Locale 
+        Locale.setDefault(new Locale("pt", "BR"));
+        
+        // Saída formatada
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+        System.out.println();
+        
+        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+        System.out.println();
+        
+       
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        
+       
+        System.out.printf("Rounded (three decimal places): %.3f%n", measure);
+        
+        
+        System.out.printf("US decimal point: %.3f%n", measure);
+    }
 }
